e5e751375d8f55629a6bd0ba8bfb3819d27441f7
Andhy Koesnandar <akoesnan@gmail.com>
2012-06-27 02:27:26 -0700

    upgrade to 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
-  match "/search" => "products#search", :as => :search
+Spree::Core::Engine.routes.draw do
+  scope :module => :spree do
+    match "/search" => "products#search", :as => :search
+  end
 end
